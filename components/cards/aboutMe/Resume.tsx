57d7c4239f117154f5ab76158d0a9806cc4b7194
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2025-11-16 14:23:26 +0300

    Version 1.67 - Minor UI update to menu toggle button. Minor update to Resume card.

--- components/cards/aboutMe/Resume.tsx
+++ components/cards/aboutMe/Resume.tsx
@@ -8,11 +8,9 @@ export default function Resume() {
         <Card title="Resumé" className="flex flex-col justify-center">
             {/* Resume contents */}
             <p className="text-lg text-gray-200 leading-7 font-light">
-                Versatile, and results-driven programmer with a wide range of interests, but mainly full-stack development as well as machine learning, deep learning, and finding new solutions for NP-complete and NP-hard problems.<br /><br />
+                Versatile and results-driven programmer with a wide range of interests, but mainly full-stack development as well as machine learning, deep learning, and finding new solutions for NP-complete and NP-hard problems.<br /><br />
                 I always strife to self-improve and learn new stuff. I have a strong background in teaching, researching, building different kinds of projects and working solo and in a team.<br /><br />
-                A researcher and instructor with patents and publications,
-                combined with strong multilingual abilities. Able to quickly adapt to new technologies and
-                environments, and work within set deadlines.
+                A researcher and instructor with patents and publications, combined with strong multilingual abilities. Able to quickly adapt to new technologies and environments, and work within set deadlines. Deeply passionate about continual learning and self-improvement.
             </p>
             {/* Social links */}
             <Link href="https://drive.google.com/file/d/1PwSQguwLiOCP_ZHCSg7bL6hOw4AKSGnn/view?usp=sharing" target="_blank" download className="ml-auto">
