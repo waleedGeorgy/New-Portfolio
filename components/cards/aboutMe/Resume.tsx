2034a13af88f689282cfcb21436ca7ef88943f44
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2026-03-11 13:12:39 +0300

    Version 1.94 - Packages cleanup. Minor code cleanup. Minor visual updates.

--- components/cards/aboutMe/Resume.tsx
+++ components/cards/aboutMe/Resume.tsx
@@ -11,8 +11,8 @@ export default function Resume() {
                 I always strife to self-improve and learn new stuff. I have a strong background in teaching, researching, building different kinds of projects and working solo and in a team.<br /><br />
                 A researcher and instructor with patents and publications, combined with strong multilingual abilities. Able to quickly adapt to new technologies and environments, and work within set deadlines. Deeply passionate about continual learning and self-improvement.
             </article>
-            <Link href="https://drive.google.com/file/d/1LpYv73t9cNIUNArugkt-n0Azwb-XrXmy/view?usp=sharing" target="_blank" download className="ml-auto">
-                <Button className="bg-blue-cosmos border border-gray-600/50 text-primary-foreground shadow-md shadow-black/50 hover:scale-110 transition-all duration-300 hover:shadow-lg hover:shadow-black/50">
+            <Link href="https://drive.google.com/file/d/1LpYv73t9cNIUNArugkt-n0Azwb-XrXmy/view?usp=sharing" target="_blank" download className="ml-auto animate-wiggle">
+                <Button className="bg-blue-cosmos border border-gray-600/50 text-gray-200 shadow-md shadow-black/50 hover:scale-110 transition-all duration-300 hover:shadow-lg hover:shadow-black/25">
                     <FaFileDownload />
                     <span>Resume</span>
                 </Button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,11 +36,7 @@ export default {
         },
         border: "rgb(255 255 255 / 0.05)",
       },
-      backgroundImage: {
-        "noise": "url(/images/noise-bg.webp)",
-      },
       animation: {
-        "noise-bg": "noise 1s steps(2) infinite",
         "grow_in": "grow_in 0.5s ease-in-out",
         wiggle: 'wiggle 2s ease-in-out infinite',
       },
@@ -56,42 +52,6 @@ export default {
           },
           '50%': {
               transform: 'rotate(2deg)'
-          },
-        },
-        
-        noise: {
-          "0%": {
-            transform: "translate3d(0, 9rem, 0)",
-          },
-          "10%": {
-            transform: "translate3d(-1rem, -4rem, 0)",
-          },
-          "20%": {
-            transform: "translate3d(-8rem, 2rem, 0)",
-          },
-          "30%": {
-            transform: "translate3d(9rem, -9rem, 0)",
-          },
-          "40%": {
-            transform: "translate3d(-2rem, 7rem, 0)",
-          },
-          "50%": {
-            transform: "translate3d(-9rem, -4rem, 0)",
-          },
-          "60%": {
-            transform: "translate3d(2rem, 6rem, 0)",
-          },
-          "70%": {
-            transform: "translate3d(7rem, -8rem, 0)",
-          },
-          "80%": {
-            transform: "translate3d(-9rem, 1rem, 0)",
-          },
-          "90%": {
-            transform: "translate3d(6rem, -5rem, 0)",
-          },
-          to: {
-            transform: "translate3d(-7rem, 0, 0)",
           },
         },
       }
