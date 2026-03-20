413735aeb0732904c1b35630d7b879df0e0c7677
Waleed Georgy <waleedgeorge1009@gmail.com>
2026-03-20 14:12:04 +0300

    Version 2.0 - Move stack list to a seperate file for better file structure.

--- components/cards/about/Stack.tsx
+++ components/cards/about/Stack.tsx
@@ -1,7 +1,6 @@
-
 import Card from "@/components/Card";
 import Tooltip from "@/components/Tooltip";
-import { myStack } from "@/data/projectsList";
+import { myStack } from "@/data/stackList";
 
 export default function Stack() {
     return (
